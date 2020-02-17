e52a57b327caea509fe5af7cb56a23e648d47c43
Lauraklijn <lauraklijn12@hotmail.com>
2020-02-17 17:15:24 +0100

    created token

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const User = require("./src/user/router-user");
+const user = require("./src/user/router-user");
 const bodyParser = require("body-parser");
 
 const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
 
-app.use(User);
+app.use(user);
 
 app.get("/test", (req, res) => {
   res.send("Hello");
--- src/user/router-user.js
+++ src/user/router-user.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const User = require("./model-user");
+const bcrypt = require("bcrypt");
+const { toJWT, toData } = require("../auth/jwt");
 
 const router = new Router();
 
@@ -10,10 +12,34 @@ router.get("/user", (req, res, next) => {
     .catch(error => next(error));
 });
 
-router.post("/user", (req, res, next) => {
-  User.create(req.body)
-    .then(user => res.json(user))
-    .catch(error => next(error));
+router.post("/user", async (req, res, next) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  try {
+    await User.create({
+      ...req.body,
+      password: hashedPassword
+    });
+    res.status(201).send("User created");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (request, response) => {
+  console.log(request.body);
+
+  const user = await User.findOne({ where: { email: request.body.email } });
+
+  const passwordValid = bcrypt.compareSync(
+    request.body.password,
+    user.password
+  );
+
+  if (passwordValid) {
+    const token = toJWT({ id: user.id });
+
+    return response.status(200).send({ token: token });
+  }
 });
 
 module.exports = router;
